75d2de824842714ce0f390f69a624f46e8e80fb3
Edug <davidugarcia007@gmail.com>
2021-04-05 21:43:46 -0600

    maquetado de seccion de educacion
    Signed-off-by: Edug <davidugarcia007@gmail.com>

--- js/script.js
+++ js/script.js
@@ -10,7 +10,7 @@ $(document).ready(function() {
 
         if (scroll > 190) {
             if (!flag) {
-                $("#image").css({ "top": "20px", "left": "122px", "width": "75px", "height": "80px" });
+                $("#image").css({ "top": "12px", "left": "185px", "width": "75px", "height": "80px" });
                 $("header").css({ "background-color": "#3C3C3C" });
                 $("li a").css({ "color": "white" });
 
@@ -18,7 +18,7 @@ $(document).ready(function() {
             }
         } else {
             if (flag) {
-                $("#image").css({ "top": "130px", "left": "127px", "width": "190px", "height": "207px" });
+                $("#image").css({ "top": "100px", "left": "127px", "width": "190px", "height": "207px" });
                 $("#picture").css({ "width": "100%" });
                 $("header").css({ "background-color": "transparent" });
                 $("li a").css({ "color": "black" });
