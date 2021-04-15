1d78d8331b56a14010a183b74e0bdf548cdac821
Edug <davidugarcia007@gmail.com>
2021-04-14 18:06:45 -0600

    modificando navbar
    Signed-off-by: Edug <davidugarcia007@gmail.com>

--- js/script.js
+++ js/script.js
@@ -8,10 +8,12 @@ $(document).ready(function() {
     $(window).scroll(function() {
         scroll = $(window).scrollTop();
 
-        if (scroll > 190) {
+        if (scroll > 97) {
             if (!flag) {
                 $("#image").css({ "top": "12px", "left": "185px", "width": "75px", "height": "80px" });
-                $("header").css({ "background-color": "#3C3C3C" });
+                $(".tituloPosicion").css({ "top": "95px" });
+                $(".header").css({ "background-color": "#3C3C3C" });
+                $(".aside").css({ "padding-top": "240px" });
                 $("nav li").css({ "border-left": "1px solid #bbb" });
                 $("nav li:last-child").css({ "border-right": "none" });
                 $("li a").css({ "color": "white" });
@@ -22,7 +24,9 @@ $(document).ready(function() {
             if (flag) {
                 $("#image").css({ "top": "100px", "left": "123px", "width": "190px", "height": "207px" });
                 $("#picture").css({ "width": "100%" });
-                $("header").css({ "background-color": "transparent" });
+                $(".header").css({ "background-color": "transparent" });
+                $(".tituloPosicion").css({ "top": "312px" });
+                $(".aside").css({ "padding-top": "350px" });
                 $("li a").css({ "color": "black" });
                 flag = false;
             }
