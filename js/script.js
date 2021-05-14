b420ffadcf6757a82ff7fd53ab86cb49b597b507
Edug <davidugarcia007@gmail.com>
2021-05-13 20:46:12 -0600

    animacion a letra de titulossss
    Signed-off-by: Edug <davidugarcia007@gmail.com>

--- js/script.js
+++ js/script.js
@@ -17,7 +17,7 @@ $(document).ready(function() {
                 $("nav li").css({ "border-left": "1px solid #bbb" });
                 $("nav li:last-child").css({ "border-right": "none" });
                 $("li a").css({ "color": "white" });
-                $(".altocontacto").css({ "height": "300px" });
+                $(".espaciocontact").css({ "height": "285px" });
 
                 flag = true;
             }
@@ -29,7 +29,7 @@ $(document).ready(function() {
                 $(".tituloPosicion").css({ "top": "312px" });
                 $(".aside").css({ "padding-top": "350px" });
                 $("li a").css({ "color": "black" });
-                $(".altocontacto").css({ "height": "200px" });
+                $(".espaciocontact").css({ "height": "200px" });
 
                 flag = false;
             }
@@ -37,3 +37,58 @@ $(document).ready(function() {
     });
 
 });
+
+class TextoAnimado {
+    constructor(id, objetivo) {
+        this.texto = document.getElementById(id);
+        console.log(this.texto)
+        this.objetivo = document.getElementById(objetivo);
+        console.log(this.objetivo)
+        this.letras = this.texto.innerText.split("");
+        console.log(this.letras)
+
+        this.texto.innerText = '';
+
+        this.letras.forEach((letra) => {
+            let caracter = letra === ' ' ? '&nbsp;' : letra;
+            console.log(caracter)
+            this.texto.innerHTML = this.texto.innerHTML + `
+                <div>
+					<span>${caracter}</span>
+					<span class="segunda-linea">${caracter}</span>
+				</div>
+			`;
+        });
+
+        this.objetivo.addEventListener('mouseenter', () => {
+            let cuenta = 0;
+
+            const intervalo = setInterval(() => {
+                if (cuenta < this.texto.children.length) {
+                    this.texto.children[cuenta].classList.add('animacion');
+                    cuenta += 1;
+                } else {
+                    clearInterval(intervalo);
+                }
+            }, 30);
+        });
+
+        this.objetivo.addEventListener('mouseleave', () => {
+            let cuenta = 0;
+
+            const intervalo = setInterval(() => {
+                if (cuenta < this.texto.children.length) {
+                    this.texto.children[cuenta].classList.remove('animacion');
+                    cuenta += 1;
+                } else {
+                    clearInterval(intervalo);
+                }
+            }, 30);
+        });
+
+    }
+}
+
+new TextoAnimado('experiencia', 'logotipo');
+new TextoAnimado('portafolio', 'portafoli');
+new TextoAnimado('contact', 'contacto');
