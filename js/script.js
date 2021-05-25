e1eb19d67e1dc006491aad424b8bc4ef0bcf8444
Edug <davidugarcia007@gmail.com>
2021-05-24 20:00:33 -0600

    codigo jquery pasandolo a javascript
    Signed-off-by: Edug <davidugarcia007@gmail.com>

--- js/script.js
+++ js/script.js
@@ -1,10 +1,141 @@
-$(document).ready(function() {
+document.addEventListener("DOMContentLoaded", function() {
+    console.log("listo");
 
+    var flag = false;
+    var scrollvertical;
+
+    window.addEventListener("scroll", function() {
+
+        scrollvertical = window.pageYOffset;
+
+        var imagen = document.getElementById("image"),
+            picture = document.getElementById("picture"),
+            tituloPosicion = document.getElementsByClassName("tituloPosicion")[0],
+            header = document.getElementsByClassName("header")[0],
+            aside = document.getElementsByClassName("aside")[0],
+            li = document.querySelectorAll("nav li"),
+            link = document.querySelectorAll("li a"),
+            espaciocontact = document.getElementsByClassName("espaciocontact")[0];
+
+        console.log(li);
+        console.log(link);
+
+
+        if (scrollvertical > 97) {
+
+            if (!flag) {
+
+                imagen.style.cssText += "top: 12px; left: 185px; width: 75px; height: 80px;";
+                tituloPosicion.style.cssText += "top: 95px;";
+                header.style.cssText += "background-color: #3C3C3C;";
+                aside.style.cssText += "padding-top: 240px;";
+
+                for (let i = 0; i < li.length; i++) {
+                    li[i].style.borderleft = "1px solid #bbb";
+                }
+
+                for (let i = 0; i < link.length; i++) {
+                    link[i].style.color = "white";
+
+                }
+
+                espaciocontact.style.cssText += "height: 285px;";
+
+                flag = true;
+            }
+
+        } else {
+
+            if (flag) {
+
+                imagen.style.cssText += "top: 100px; left: 123px; width: 190px; height: 207px;";
+                picture.style.cssText += "width: 100%;";
+                tituloPosicion.style.cssText += "top: 312px";
+                header.style.cssText += "background-color: transparent;";
+                aside.style.cssText += "padding-top: 350px;";
+
+                for (let i = 0; i < link.length; i++) {
+                    link[i].style.color = "black";
+
+                }
+
+                espaciocontact.style.cssText += "height: 200px;";
+
+                flag = false;
+            }
+        }
+
+    });
+
+});
+
+
+/*
+            class TextoAnimado {
+                constructor(id, objetivo) {
+                    this.texto = document.getElementById(id);
+                    //console.log(this.texto)
+                    this.objetivo = document.getElementById(objetivo);
+                    //console.log(this.objetivo)
+                    this.letras = this.texto.innerText.split("");
+                    //console.log(this.letras)
+
+                    this.texto.innerText = '';
+
+                    this.letras.forEach((letra) => {
+                        let caracter = letra === ' ' ? '&nbsp;' : letra;
+                        //console.log(caracter)
+                        this.texto.innerHTML = this.texto.innerHTML + `
+                <div>
+					<span>${caracter}</span>
+					<span class="segunda-linea">${caracter}</span>
+				</div>
+			`;
+                    });
+
+                    this.objetivo.addEventListener('mouseenter', () => {
+                        let cuenta = 0;
+
+                        const intervalo = setInterval(() => {
+                            if (cuenta < this.texto.children.length) {
+                                this.texto.children[cuenta].classList.add('animacion');
+                                cuenta += 1;
+                            } else {
+                                clearInterval(intervalo);
+                            }
+                        }, 30);
+                    });
+
+                    this.objetivo.addEventListener('mouseleave', () => {
+                        let cuenta = 0;
+
+                        const intervalo = setInterval(() => {
+                            if (cuenta < this.texto.children.length) {
+                                this.texto.children[cuenta].classList.remove('animacion');
+                                cuenta += 1;
+                            } else {
+                                clearInterval(intervalo);
+                            }
+                        }, 30);
+                    });
+
+                }
+            }
+
+new TextoAnimado('experiencia', 'logotipo');
+new TextoAnimado('portafolio', 'portafoli');
+new TextoAnimado('contact', 'contacto');*/
+
+/*
+let ancho = screen.width;
+console.log(ancho);*/
+
+/*$(document).ready(function() {
 
     var flag = false;
     var scroll;
 
-    $(window).scroll(function() {
+   $(window).scroll(function() {
         scroll = $(window).scrollTop();
 
         if (scroll > 97) {
@@ -18,7 +149,6 @@ $(document).ready(function() {
                 $("nav li:last-child").css({ "border-right": "none" });
                 $("li a").css({ "color": "white" });
                 $(".espaciocontact").css({ "height": "285px" });
-
                 flag = true;
             }
         } else {
@@ -36,59 +166,4 @@ $(document).ready(function() {
         }
     });
 
-});
-
-class TextoAnimado {
-    constructor(id, objetivo) {
-        this.texto = document.getElementById(id);
-        //console.log(this.texto)
-        this.objetivo = document.getElementById(objetivo);
-        //console.log(this.objetivo)
-        this.letras = this.texto.innerText.split("");
-        //console.log(this.letras)
-
-        this.texto.innerText = '';
-
-        this.letras.forEach((letra) => {
-            let caracter = letra === ' ' ? '&nbsp;' : letra;
-            //console.log(caracter)
-            this.texto.innerHTML = this.texto.innerHTML + `
-                <div>
-					<span>${caracter}</span>
-					<span class="segunda-linea">${caracter}</span>
-				</div>
-			`;
-        });
-
-        this.objetivo.addEventListener('mouseenter', () => {
-            let cuenta = 0;
-
-            const intervalo = setInterval(() => {
-                if (cuenta < this.texto.children.length) {
-                    this.texto.children[cuenta].classList.add('animacion');
-                    cuenta += 1;
-                } else {
-                    clearInterval(intervalo);
-                }
-            }, 30);
-        });
-
-        this.objetivo.addEventListener('mouseleave', () => {
-            let cuenta = 0;
-
-            const intervalo = setInterval(() => {
-                if (cuenta < this.texto.children.length) {
-                    this.texto.children[cuenta].classList.remove('animacion');
-                    cuenta += 1;
-                } else {
-                    clearInterval(intervalo);
-                }
-            }, 30);
-        });
-
-    }
-}
-
-new TextoAnimado('experiencia', 'logotipo');
-new TextoAnimado('portafolio', 'portafoli');
-new TextoAnimado('contact', 'contacto');
+});*/
